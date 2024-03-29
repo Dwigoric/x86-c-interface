26435dc6f837ffe3d6b2d15ad2192a9de56bcaba
Joshua Permito <dwigoric@insanedevs.com>
2024-03-29 22:38:24 +0800

    refactor(asm): remove unnecessary logic

--- SAXPY.asm
+++ SAXPY.asm
@@ -1,10 +1,8 @@
 section .data
 
 invalid_input db "Invalid input: n must be greater than 0", 10, 0
-test_str db "RCX is %d", 10, 0
 
 section .text
-bits 64
 default rel
 global saxpy_asm
 extern printf, malloc
@@ -22,8 +20,6 @@ saxpy_asm:
 CONTINUE:
 	; Z[i] = A * X[i] + Y[i]
 	; ecx = n, xmm0 = A, rdx = *X, r8 = *Y
-        
-	mov rdx, rcx
 
 	; allocate memory for Z
 	sub rsp, 8*5
